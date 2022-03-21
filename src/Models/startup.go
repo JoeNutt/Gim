c0547e0ca24bb6a413effe58d2f39f54d2e427d9
Joseph-Nutt <joenutt01@gmail.com>
2022-03-21 23:51:47 +0000

    add git and git files

---
+++ src/Models/startup.go
@@ -0,0 +1,7 @@
+package models
+
+type model struct {
+	choices  []string         // items on the to-do list
+	cursor   int              // which to-do list item our cursor is pointing at
+	selected map[int]struct{} // which to-do items are selected
+}
